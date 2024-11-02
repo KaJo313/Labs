04ae09ef15d29aa861ac56ea323aa006c4e29ef5
Kevin Johnson <johnson.kevin.a@icloud.com>
2024-11-01 21:34:57 -0400

    completed lab-5

---
+++ lab-5/index.js
@@ -0,0 +1,25 @@
+const firstName = "Kevin"
+const lastName = "Johnson"
+const fullName = firstName + " " + lastName
+console.log(fullName)
+const populationDetroit = 633218
+console.log(populationDetroit)
+console.log(populationDetroit*3)
+const javaScriptIsCool = true
+const colors = [
+    "red", 
+    "orange", 
+    "yellow", 
+    "green", 
+    "blue", 
+    "indigo",
+    "violet"
+]
+let i = 5
+for (let i=1; i<=5; i++) {
+    console.log(i)
+}
+for (let i=0; i<=4; i++) {
+    console.log(i)
+}
+
