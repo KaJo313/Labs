3d4f7dffdc7cac1264ecd4ca7e70e2aa761f2b79
Kevin A. Johnson <118540893+KaJo313@users.noreply.github.com>
2024-11-13 10:10:59 -0500

    Update unit-3-js-labs/counting-loops/index.js
    Co-authored-by: AJ Tran <31839316+ajtran303@users.noreply.github.com>

--- unit-3-js-labs/counting-loops/index.js
+++ unit-3-js-labs/counting-loops/index.js
@@ -1,8 +1,9 @@
 for (let i = 1; i<=10; i++) {
     console.log (i)    
-    for (let j = 10; j >= 1; j--) {  
-        console.log(j)
-    }
+}
+
+for (let j = 10; j >= 1; j--) {  
+    console.log(j)
 }
 
 
