64f6d3d3faa857553b4993969d4431d94304ce50
Kevin A. Johnson <118540893+KaJo313@users.noreply.github.com>
2024-11-13 10:11:35 -0500

    Update unit-3-js-labs/counting-loops/index.js
    Co-authored-by: AJ Tran <31839316+ajtran303@users.noreply.github.com>

--- unit-3-js-labs/counting-loops/index.js
+++ unit-3-js-labs/counting-loops/index.js
@@ -34,10 +34,9 @@ do {
 
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for (let p = 0; p < numbers.length; p++) {
-    const number = numbers[p]
-	console.log(number)
-}
+for (const number of numbers) {
+    console.log(number)
+} 
 
 
 anyString = "headlamp"
